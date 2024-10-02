df2dd70fbff302b8e5f76c27fdc4eafe446bd82a
KIMSEI1124 <workju1124@gmail.com>
2024-10-02 19:15:21 +0900

    파일 정리

--- 240912/dispatcherservlet/src/main/java/com/seikim/dispatcherservlet/data/Base64Request.java
+++ 240912/dispatcherservlet/src/main/java/com/seikim/dispatcherservlet/data/Base64Request.java
@@ -2,7 +2,7 @@ package com.seikim.dispatcherservlet.data;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public record Base64Request(
+public record  Base64Request(
 		@JsonProperty("value") String value
 ) {
 }
