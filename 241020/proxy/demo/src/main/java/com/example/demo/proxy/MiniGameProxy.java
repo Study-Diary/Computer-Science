84ea0aa3c7d6f3496cb0271f112c6cea2cbe1e96
jiwonhan <jiwons0803@naver.com>
2024-10-20 16:27:52 +0900

    Feat: 여러 RealSubject가 있을 떄

--- 241020/proxy/demo/src/main/java/com/example/demo/proxy/MiniGameProxy.java
+++ 241020/proxy/demo/src/main/java/com/example/demo/proxy/MiniGameProxy.java
@@ -1,14 +1,18 @@
 package com.example.demo.proxy;
 
+import java.lang.reflect.InvocationTargetException;
+
 public class MiniGameProxy implements CanMiniGame {
 
-    private MiniGame realMiniGame;
-
+//    private MiniGame realMiniGame;
+    private CanMiniGame realMiniGame;
+    private String className; // Real MiniGame 객체의 클래스 이름
     private String userName;
 
-    public MiniGameProxy() {
-        realMiniGame = null; // 객체 생성을 지연
-        userName = "null";
+    public MiniGameProxy(String className) {
+        this.realMiniGame = null; // 객체 생성을 지연
+        this.className = className;
+        this.userName = "null";
     }
 
     @Override
@@ -23,7 +27,7 @@ public class MiniGameProxy implements CanMiniGame {
     @Override
     public void welcomeMessage() {
         if (realMiniGame == null){
-            System.out.println(userName+ "님, MiniGame 에 오신 것을 환영합니다 : ) ");
+            System.out.println(userName+ "님, "+ className + " 에 오신 것을 환영합니다 : ) ");
         }else{
             realMiniGame.welcomeMessage();
         }
@@ -32,7 +36,12 @@ public class MiniGameProxy implements CanMiniGame {
     @Override
     public void playGame() {
         if (realMiniGame == null){
-            realMiniGame = new MiniGame(userName);
+            try {
+                realMiniGame = (CanMiniGame) Class.forName("com.example.demo.proxy."+ className).getDeclaredConstructor().newInstance();
+            } catch (ClassNotFoundException | NoSuchMethodException | InstantiationException | IllegalAccessException |
+                     InvocationTargetException e) {
+                throw new RuntimeException(e);
+            }
         }
         realMiniGame.playGame();
     }
--- 241020/proxy/demo/src/test/java/com/example/demo/proxy/MiniGameProxyTest.java
+++ 241020/proxy/demo/src/test/java/com/example/demo/proxy/MiniGameProxyTest.java
@@ -8,7 +8,7 @@ class MiniGameProxyTest {
 
     @Test
     void miniGameTestProxy(){
-        CanMiniGame miniGame = new MiniGameProxy();
+        CanMiniGame miniGame = new MiniGameProxy("MiniGame");
         miniGame.setUserName("쥴리"); // 실제 객체는 생성되지 않았다.
         miniGame.welcomeMessage();
 
@@ -22,9 +22,27 @@ class MiniGameProxyTest {
 
     @Test
     void onlyWelcomeProxy(){
-        CanMiniGame miniGame = new MiniGameProxy();
+        CanMiniGame miniGame = new MiniGameProxy("MiniGame");
         miniGame.setUserName("쥴리"); // 실제 객체는 생성되지 않았다.
         miniGame.welcomeMessage();
+    }
+
+    @Test
+    void proxyTestWhenVariousMiniGame(){
+        CanMiniGame miniGame = new MiniGameProxy("PuzzleMiniGame");
+        miniGame.setUserName("쥴리"); // 실제 객체는 생성되지 않았다.
+        miniGame.welcomeMessage();
+        miniGame.playGame();
+
+        CanMiniGame miniGame2 = new MiniGameProxy("BubbleMiniGame");
+        miniGame2.setUserName("나띠"); // 실제 객체는 생성되지 않았다.
+        miniGame2.welcomeMessage();
+        miniGame2.playGame();
+
+        CanMiniGame miniGame3 = new MiniGameProxy("MarioMiniGame");
+        miniGame3.setUserName("벨"); // 실제 객체는 생성되지 않았다.
+        miniGame3.welcomeMessage();
+        miniGame3.playGame();
     }
 
 }
